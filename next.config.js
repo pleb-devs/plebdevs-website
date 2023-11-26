ba40afd896946c42e372305b1d4e4bb0155e11b3
austinkelsay <austinkelsay@yahoo.com>
2023-11-26 13:47:15 -0600

    Cors headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
+const corsHeaders = [
+  {
+    key: "Access-Control-Allow-Origin",
+    value: "*",
+  },
+  {
+    key: "Access-Control-Allow-Methods",
+    value: "GET, HEAD, OPTIONS",
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['i.ytimg.com'],
   },
+  async headers() {
+    return [
+      {
+        source: "/.well-known/:slug*",
+        headers: [...corsHeaders],
+      },
+    ];
+  },
+
   async rewrites() {
     return [
       {
