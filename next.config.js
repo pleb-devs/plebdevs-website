fa295554050998e07af2589950eb8b5c433febfc
austinkelsay <austinkelsay@yahoo.com>
2024-11-03 13:17:51 -0600

    Added a redirects configuration to handle the domain redirect with CORS headers

--- next.config.js
+++ next.config.js
@@ -1,55 +1,48 @@
 const corsHeaders = [
-  {
-    key: "Access-Control-Allow-Origin",
-    value: "*",
-  },
-  {
-    key: "Access-Control-Allow-Methods",
-    value: "GET, HEAD, OPTIONS",
-  },
-  {
-    key: "Access-Control-Allow-Headers",
-    value: "Content-Type, Authorization",
-  },
+  { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  { key: 'Access-Control-Allow-Origin', value: '*' },
+  { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
+  { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
 ];
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['i.ytimg.com'],
+  async redirects() {
+    return [
+      {
+        source: '/.well-known/nostr.json',
+        has: [
+          {
+            type: 'host',
+            value: 'plebdevs.com',
+          },
+        ],
+        destination: 'https://www.plebdevs.com/.well-known/nostr.json',
+        permanent: true,
+        headers: corsHeaders,
+      },
+    ];
   },
   async headers() {
     return [
       {
         source: '/api/nostr/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
+        headers: corsHeaders,
       },
       {
         source: '/.well-known/nostr.json',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      }
+        headers: corsHeaders,
+      },
     ];
   },
-
   async rewrites() {
     return [
       {
-        source: "/.well-known/nostr.json",
-        destination: "/api/nostr/nip05"
+        source: '/.well-known/nostr.json',
+        destination: '/api/nostr/nip05',
       },
     ];
   }
-}
+};
 
 module.exports = nextConfig;
