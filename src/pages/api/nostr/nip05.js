ff7ba80ae6c7271a8c3c4389e18a09f9b39576c0
austinkelsay <austinkelsay@yahoo.com>
2024-11-03 13:12:32 -0600

    return status 200

--- src/pages/api/nostr/nip05.js
+++ src/pages/api/nostr/nip05.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
   }
 
   if (req.method === 'GET') {
-    return res.json(nostrData);
+    return res.status(200).json(nostrData);
   }
 
   return res.status(405).json({ error: 'Method not allowed' });
